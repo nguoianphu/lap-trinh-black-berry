ef3949f041a23f6c7aa089f6630e7d5f2e379b8a
Nguoi An Phu <nguoianphu@gmail.com>
2012-05-25 10:57:00 +0700

    complete nap03 + new FileConnection

---
+++ FileConnection/src/mypackage/FileConnectionApp.java
@@ -0,0 +1,32 @@
+package mypackage;
+
+import net.rim.device.api.ui.UiApplication;
+
+/**
+ * This class extends the UiApplication class, providing a
+ * graphical user interface.
+ */
+public class FileConnectionApp extends UiApplication
+{
+    /**
+     * Entry point for application
+     * @param args Command line arguments (not used)
+     */ 
+    public static void main(String[] args)
+    {
+        // Create a new instance of the application and make the currently
+        // running thread the application's event dispatch thread.
+        FileConnectionApp theApp = new FileConnectionApp();       
+        theApp.enterEventDispatcher();
+    }
+    
+
+    /**
+     * Creates a new MyApp object
+     */
+    public FileConnectionApp()
+    {        
+        // Push a screen onto the UI stack for rendering.
+        pushScreen(new FileConnectionScreen());
+    }    
+}
---
+++ nap03 Luu_du_lieu/src/mypackage/CustomHashtable.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+import java.util.Hashtable;
+
+import net.rim.device.api.util.Persistable;
+
+public class CustomHashtable extends Hashtable implements Persistable{
+
+}
